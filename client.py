88d2b1024b5b42acda72838bfc35fc888a6f30e6
tushar1572005 <tusharchaudhari1572005@gmail.com>
2025-04-14 10:15:02 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 client = OpenAI(
-    api_key="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",
+    api_key="<YOUR API KEY>",
 )
 
 comletion = client.chat.completions.create(
